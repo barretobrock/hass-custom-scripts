6c2fdf60cafa0b6e0b2040bf430105115a230b04
Barret Obrock <barret@tarkmail.com>
2025-02-20 17:17:17 -0600

    Add measurement class to nws sensors

--- nws_min_temps.py
+++ nws_min_temps.py
@@ -10,6 +10,10 @@ import datetime
 import json
 import os
 import pathlib
+from typing import (
+    Dict,
+    Union
+)
 
 from dotenv import load_dotenv
 import requests
@@ -18,17 +22,33 @@ import requests
 ROOT = pathlib.Path(__file__).parent
 load_dotenv(dotenv_path=ROOT.joinpath('.env'))
 
+HASS_DOMAIN = os.environ['HASS_DOMAIN']
+HASS_TOKEN = os.environ['HASS_TOKEN']
+NWS_STATION = os.environ['NWS_STATION_ID']
+NWS_POINTS = os.environ['NWS_POINTS']
+
+def post_sensor(sensor_name: str, state: Union[str, float], attributes: Dict[str, Union[str, float]]):
+    url = f'http://{HASS_DOMAIN}/api/states/sensor.{sensor_name}'
+    requests.post(
+        url,
+        headers={
+            'Authorization': f'Bearer {HASS_TOKEN}',
+            'content-type': 'application/json'
+        },
+        data=json.dumps({
+            'state': state,
+            'attributes': attributes
+        })
+    )
+
 def convert_to_c(temp_f: float) -> float:
     return (temp_f - 32) * (5/9)
 
 def gather_data():
     NEXT_N_HOURS = 18
-    hass_domain = os.environ['HASS_DOMAIN']
-    hass_token = os.environ['HASS_TOKEN']
-    nws_station = os.environ['NWS_STATION_ID']
-    nws_points = os.environ['NWS_POINTS']
 
-    resp = requests.get(f"https://api.weather.gov/gridpoints/{nws_station}/{nws_points}/forecast/hourly")
+
+    resp = requests.get(f"https://api.weather.gov/gridpoints/{NWS_STATION}/{NWS_POINTS}/forecast/hourly")
     resp.raise_for_status()
 
     data = resp.json()
@@ -54,42 +74,34 @@ def gather_data():
 
     min_temp = min(temps)
 
-    # Send data to HASS
-    resp = requests.post(
-        f'http://{hass_domain}/api/states/sensor.nws_upcoming_min_temp',
-        headers={
-            'Authorization': f'Bearer {hass_token}',
-            'content-type': 'application/json'
-        },
-        data=json.dumps({
-            'state': round(min_temp, 1),
-            'attributes': {
-                'unit_of_measurement': '°C',
-                'friendly_name': 'NWS Upcoming Min Temp',
-                'freezing_temp_starts': freezing_temps_start_timestamp,
-                'freezing_temp_ends': freezing_temps_end_timestamp,
-                'freezing_temp_duration': freezing_temps_duration
-            }
-        })
+    # Send upcoming min temp
+    post_sensor(
+        'nws_upcoming_min_temp',
+        round(min_temp, 1),
+        attributes={
+            'unit_of_measurement': '°C',
+            'state_class': 'measurement',
+            'friendly_name': 'NWS Upcoming Min Temp',
+            'freezing_temp_starts': freezing_temps_start_timestamp,
+            'freezing_temp_ends': freezing_temps_end_timestamp,
+            'freezing_temp_duration': freezing_temps_duration
+        }
     )
+
     # Send current weather detail
-    resp = requests.post(
-        f'http://{hass_domain}/api/states/sensor.nws_current',
-        headers={
-            'Authorization': f'Bearer {hass_token}',
-            'content-type': 'application/json'
-        },
-        data=json.dumps({
-            'state': round(convert_to_c(current_period['temperature']), 1),
-            'attributes': {
-                'unit_of_measurement': '°C',
-                'wind_speed_mph': round(float(current_period['windSpeed'].replace(' mph', '')), 1),
-                'short_forecast': current_period['shortForecast'],
-                'humidity': current_period['relativeHumidity']['value'],
-                'dewpoint': round(current_period['dewpoint']['value'], 1),
-                'precip_probability': round(current_period['probabilityOfPrecipitation']['value'])
-            }
-        })
+    post_sensor(
+        'nws_current',
+        round(convert_to_c(current_period['temperature']), 1),
+        attributes={
+            'unit_of_measurement': '°C',
+            'state_class': 'measurement',
+            'friendly_name': 'NWS Current Weather',
+            'wind_speed_mph': round(float(current_period['windSpeed'].replace(' mph', '')), 1),
+            'short_forecast': current_period['shortForecast'],
+            'humidity': current_period['relativeHumidity']['value'],
+            'dewpoint': round(current_period['dewpoint']['value'], 1),
+            'precip_probability': round(current_period['probabilityOfPrecipitation']['value'])
+        }
     )
 
 
